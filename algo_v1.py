4d8784a434f5d3fc583829aa47526074ac10bcc2
Andrea Fabbricatore <andy.fabb10@gmail.com>
2023-03-21 19:45:24 +0100

    started dev of alg1 for bananas

--- algo_v1.py
+++ algo_v1.py
@@ -8,13 +8,32 @@ class Trader:
 
         for product in state.order_depths.keys():
             if product == "PEARLS":
-                pass
+                order_depth: OrderDepth = state.order_depths[product]
+                pearl_orders: list[Order] = []
+
             elif product == "BANANAS":
                 # init lists
                 order_depth: OrderDepth = state.order_depths[product]
-                banana_orderrs: list[Order] = []
+                banana_orders: list[Order] = []
 
                 # check whether to ask or bid first based on volume
                 # to be implemented?
+
+                # getting ask, bid prices
+                best_ask = min(order_depth.sell_orders.keys())
+                best_bid = max(order_depth.buy_orders.keys())
+
+                # if best_bid < best_ask go
+                price_change = 0
+                if best_bid < best_ask:
+                    price_change = (best_ask - best_bid) / 4
+
+                our_bid = best_bid + price_change
+                our_ask = best_ask - price_change
+
+                banana_orders.append(Order(product, our_bid, 5))
+                banana_orders.append(Order(product, our_ask, -5))
+
+                result[product] = banana_orders
 
         return result
